794463a1dd6b0114dda7de450a73d3bb3ac9841c
Fernando Rodrigues <fernando_tecti@hotmail.com>
2023-02-26 11:24:25 -0300

    pasta pages adicionada e proteção ao gerar as variáveis no controller

--- app/_core/Controller.php
+++ app/_core/Controller.php
@@ -8,25 +8,48 @@ class Controller
     public function render(string $page, array $data = [])
     {
         if ($data) {
+            foreach ($data as $key => $value) {
+                if (isset($$key)) {
+                    echo "Erro: Não é possível redeclarar a variável $" . $key;
+                    exit;
+                }
+            }
             extract($data);
         }
 
-        $path = dirname(__FILE__, 2) . "/mvc/views/";
-
-        require $path . $page . ".php";
-        exit;
+        $path = dirname(__FILE__, 2) . "/mvc/views/pages/";
+        $file = $path . $page . ".php";
+        if (file_exists($file)) {
+            require $file;
+            exit;
+        } else {
+            echo "Erro ao carregar a VIEW: " . $page;
+            exit;
+        }
     }
 
     // $data estará disponível como variáveis ao renderizar o partial
     public function partial(string $page, array $data = [])
     {
         if ($data) {
+            foreach ($data as $key => $value) {
+                if (isset($$key)) {
+                    echo "Erro: Não é possível redeclarar a variável $" . $key;
+                    exit;
+                }
+            }
             extract($data);
         }
 
         $path = dirname(__FILE__, 2) . "/mvc/views/partials/";
+        $file = $path . $page . ".php";
 
-        require $path . $page . ".php";
+        if (file_exists($file)) {
+            require $file;
+        } else {
+            echo "Erro ao carregar a PARTIAL: " . $page;
+            exit;
+        }
     }
 
     public function redirect($route)
